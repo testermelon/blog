28d97060ab8c85e3dafcdf7bc24b5063ba80e60b
Indra Nur Adisusilo <indrahebat@hotmail.com>
2019-11-17 16:31:56 +0900

    Revamped structure to be single directory
    moved index.php to blog directory, requiring redirect to point to it.
    This way it is cleaner for the blog app

--- defaults.php
+++ defaults.php
@@ -1,6 +1,10 @@
 <?php
 
 return array(
-	'dataroot'=>'blog/content/',
-	'csspath'=>'blog/theme.css',
+	//dataroot is the path to content of the blog relative from index.php
+	'dataroot'=>'content/',
+	//path to css file relative from domain name
+	'csspath'=> 'blog/theme.css',
 );
+
+?>
---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> <?php
+		//check if config file exists, if exist, include it, if not load default config file
+		if(file_exists('/config.php'))
+			$config = include('config.php');
+		else
+			$config = include('defaults.php');
+		
+		include(dirname(__FILE__) . '/renderer.php');
+
+		if(isset($_GET['category']))
+			echo "testermelon - ". $_GET['category'];
+		else
+		if(isset($_GET['article'])){
+			$content = get_article_content($_GET['article'],$config['dataroot']);
+			echo $content['title'];
+		}
+		else
+			echo "testermelon - Home";
+	?> </title>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" type="text/css" 
+		href="<?php 
+		//using '//' to force the link as absolute reference to file
+		echo '//' . $_SERVER['SERVER_NAME'] . '/' . $config['csspath']; 
+	?>">
+
+	<meta property="og:image" 
+		content="<?php
+		//load image for social media if set. If not load default thumbnail image
+		if (isset($content['thumbnail']))
+			echo $content['thumbnail'];
+		else 
+			echo 'http://testermelon.com/img/testermelon-banner.png';
+	?>" />
+
+	<meta property="og:type" content="article" />
+</head>
+<body>
+	<header>
+		<h1> <a href="/"> <img src="/img/top-logo.png" alt="~testermelon" /> </a> </h1>
+	<?php
+		show_menu($config['dataroot']);
+	?>
+	</header>
+
+	<div class="main">
+	<?php
+		if(isset($_GET['category']))
+			show_category($_GET['category'],$config['dataroot']);
+		else
+		if(isset($_GET['article'])){
+			show_article($content,$config['dataroot']);
+		}
+		else{
+			show_recent($config['dataroot']);
+	}
+	?>
+	</div>
+
+	<footer>
+		<ul class="navi">
+			<li><a href="/article/about" >About Me</a></li>
+		</ul>
+	</footer>
+
+</body>
+</html>

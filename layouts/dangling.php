4c4deeda9ffbb03f2cb0c82d47b893263460dea3
testermelon <testermelon@protonmail.com>
2022-02-13 21:42:46 +0900

    add dangling layout

---
+++ layouts/dangling.php
@@ -0,0 +1,22 @@
+<?php
+
+function render($data,$target_path,$config){
+	//html head data
+	$htmlcontent['active-css'] = $config['csspath'];
+	$htmlcontent['title'] = $data['title'];
+	$htmlcontent['thumbnail'] = print_head_thumbnail($config['imgpath'],$data['thumbnail']);
+	$htmlcontent['head-js-modules'] = print_head_js_modules($data['jsmod']);
+
+	//html body data
+	//$htmlcontent['header'] = print_menu($config['dataroot'],$target_path,$config);
+	$htmlcontent['main'] .= '<h1>'. $data['title'] . '</h1>';
+	$htmlcontent['main'] .= print_article_body($data,$config['imgpath']);
+	//$htmlcontent['main'] .= print_article_nav_away($config['dataroot'],$target_path);
+
+	//call template to print html response out
+	include("templates/basic.php");
+
+}
+
+?>
+
